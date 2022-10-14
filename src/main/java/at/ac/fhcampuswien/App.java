d7fed27b0ac1788338ab9a8bbaf324877d1b0066
ManuelStefanAmbros <manuel.ambros@stud.fh-campuswien.ac.at>
2022-10-14 20:02:31 +0200

    Task 4 finished; Task 1-3 set to public; Task 1, 2 corrected; Task § posting question;

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -4,7 +4,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class App {
-    static void oneMonthCalendar(int totalDays, int startDay){
+    public static void oneMonthCalendar(int totalDays, int startDay){
         int dayCount = 1;
         int row = 0;
 
@@ -19,24 +19,29 @@ public class App {
                     System.out.print(dayCount + " ");}
                 row ++;
                 dayCount++;
-                if (row == 7){
+                if (row == 7 || dayCount > totalDays){
                     System.out.print("\n");
                     row = 0;}
             }
         }
     }
 
-    static long lcg(long input){
+    public static long[] lcg(long input){
         int m = 2;
         int c = 12345;
         int a = 1103515245;
+        long[] output = {1,2,3,4,5,6,7,8,9,10};
+        long outputs = input;
         for(int i = 1; i < 31; i++)
             m *= 2;
-        return (input * a + c) % m;
+        for(int j = 0; j < 10; j++){
+            outputs = (outputs * a + c) % m;
+            output[j] = outputs;
+        }
+        return output;
     }
 
-    static void guessingGame(int numberToGuess){
-        int numberAI = randomNumberBetweenOneAndHundred();
+    public static void guessingGame(int numberToGuess){
         int guessCounter = 1;
         int userGuess = -1;
         Scanner numberUser = new Scanner(System.in);
@@ -44,14 +49,14 @@ public class App {
         for(int i = 1; i <= 10; i++){
             System.out.print("Guess number " + guessCounter + ": ");
             userGuess = numberUser.nextInt();
-            if (userGuess == numberAI){
+            if (userGuess == numberToGuess){
                 System.out.println("You won wisenheimer!");
                 return;}
             else{
                 if(guessCounter == 10){
                     System.out.println("You lost! Have you ever heard of divide & conquer?");
                     return;}
-                if(userGuess > numberAI){
+                if(userGuess > numberToGuess){
                     System.out.println("The number AI picked is lower than your guess.");}
                 else{
                     System.out.println("The number AI picked is higher than your guess.");}
@@ -59,11 +64,21 @@ public class App {
             }
         }
     }
-    static int randomNumberBetweenOneAndHundred(){
+    public static int randomNumberBetweenOneAndHundred(){
         Random number = new Random();
-        return number.nextInt(100);
+        return number.nextInt(100) + 1;
     }
-
+    public static boolean swapArrays(int [] array1, int [] array2){
+        if (array1.length == array2.length){
+            int variable = 0;
+            for(int i = 0; i < array1.length; i++){
+                variable = array1[i];
+                array1[i] = array2[i];
+                array2[i] = variable;}
+            return true;}
+        else{
+            return false;}
+    }
     // Implement all methods as public static
 
     public static void main(String[] args) {
